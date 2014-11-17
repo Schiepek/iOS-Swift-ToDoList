acaefcd2b3d9f46aeeb1e7bd4a784093a8445b68
Hans Muster <admin@Hanss-Mac.local>
2014-11-17 11:32:19 +0100

    Commit 2

--- MyTaskList/AppDelegate.swift
+++ MyTaskList/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var x = 1
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
